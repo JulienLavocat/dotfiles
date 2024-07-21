e09695ada855e37f8ca75f071515f742535858e2
Julien Lavocat <julien.lavocat@gmail.com>
2024-07-21 13:33:35 +0200

    feat: add details to killonport command

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,5 +1,7 @@
 # Kill a process runing on the specified port
 # @usage killonport [PORT]
 function killonport() {
-  sudo kill -9 `sudo lsof -t -i:$1`
+  sudo lsof -i:$1
+  port=$(sudo lsof -t -i:$1)
+  sudo kill -9 $port
 }
