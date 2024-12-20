5282db0d70fa02b5b2e6cadd8b0dd418100c4f16
Julien Lavocat <julien.lavocat@gmail.com>
2024-12-20 11:25:23 +0100

    feat: group empty directories into one line in neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,19 +1,18 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          ".git",
-          ".DS_Store",
-          "thumbs.db",
-        },
-        never_show = {},
+  opts = function(_, opts)
+    opts.filesystem.filtered_items = {
+      visible = true,
+      show_hidden_count = true,
+      hide_dotfiles = false,
+      hide_gitignored = true,
+      hide_by_name = {
+        ".git",
+        ".DS_Store",
+        "thumbs.db",
       },
-    },
-  },
+      never_show = {},
+    }
+    opts.filesystem.group_empty_dirs = true
+  end,
 }
