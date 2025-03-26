0e24044dfff80c19522fcae77b231984035e9438
Julien Lavocat <julien.lavocat@gmail.com>
2025-03-26 11:46:55 +0100

    feat: disable inlay-hint and update copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,7 +3,20 @@ return {
   cmd = "Copilot",
   build = ":Copilot auth",
   opts = {
-    suggestion = { enabled = true, auto_trigger = true },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      hide_during_completion = true,
+      debounce = 75,
+      keymap = {
+        accept = "<M-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+    },
     panel = { enabled = false },
     filetypes = {
       markdown = true,
