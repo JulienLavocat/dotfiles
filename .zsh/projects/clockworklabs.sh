cc44db19f5b296192dd18484b337b777a00a5f0d
Julien Lavocat <julien.lavocat@gmail.com>
2026-07-11 13:13:51 +0200

    feat(projects): update clockworklabs commands

--- .zsh/projects/clockworklabs.sh
+++ .zsh/projects/clockworklabs.sh
@@ -1,33 +1,23 @@
-function cl-web() {
-    BASE_PATH=~/clockworklabs/web
+function update-cloud-julien() {
+    cargo build --release
+    cargo controldb build
 
-    # Run all databases, except the web server
-    SERVICES=(pgsql prometheus)
-    docker compose -f $BASE_PATH/docker-compose.yml up -d $SERVICES
+    scp /home/julien/clockworklabs/spacetimedbprivate/target/wasm32-unknown-unknown/release-wasm/spacetime_control.wasm root@stdb:/stdb
+    scp /home/julien/clockworklabs/spacetimedbprivate/target/release/spacetimedb-cloud root@stdb:/stdb
 
-    tmux rename-window 'nvim-stdb-web'
-    tmux send-keys "cd $BASE_PATH/spacetimedb && nvim" Enter
-
-    # Run the web server
-    tmux new-window -n 'stdb-web'
-    tmux send-keys "cd $BASE_PATH/spacetimedb.com && pnpm dev" Enter
-
-    # Add a new pane to run commands
-    tmux split-window
+    echo "Build and upload complete. Now run the following commands on the server:"
+    echo "service spacetimedb restart"
 }
 
 function cl-login-local() {
-    spacetime logout
     spacetime login --auth-host http://localhost:3000
 }
 
 function cl-login-stg() {
-    spacetime logout
     spacetime login --auth-host https://staging.spacetimedb.com
 }
 
 function cl-login-prod() {
-    spacetime logout
     spacetime login
 }
 
